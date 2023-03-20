fcfd359f772f9ba16ca09ea9040078fb52c66849
Arieleon25 <Arieleon25@yahoo.com>
2023-03-20 18:04:57 -0400

    Add project files.

---
+++ ParsingEnums/DaysofWeek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public class DaysofWeek
+    {
+        public Day Day { get; set; }
+    }
+    public enum Day
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+}
---
+++ ParsingEnums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysofWeek week = new DaysofWeek();
+
+            Console.WriteLine("Enter the current day of the week");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                week.Day = (Day)Enum.Parse(typeof(Day), userInput);
+                Console.WriteLine("You said: " + week.Day);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
